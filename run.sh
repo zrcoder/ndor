61f09ad65d0eae0f47a6ebd043992b73be69e5f4
zrcoder <love-nankai@163.com>
2023-02-14 09:47:16 +0800

    small fix

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
 wwwDir=./static
-monacoEditorDir=${wwwDir}/monaco-editor
 serverDir=./server
 
 cp -f "$(go env GOROOT)/misc/wasm/wasm_exec.js" ${wwwDir}
-GOOS=js GOARCH=wasm go build -o ./static/niudour.wasm .
+GOOS=js GOARCH=wasm go build -o ${wwwDir}/niudour.wasm .
 go run ${serverDir}
