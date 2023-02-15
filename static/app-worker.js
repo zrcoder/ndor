71bcfe99e5b64533f6acfcb8462cd50ec525282c
zrcoder <love-nankai@163.com>
2023-02-15 13:55:59 +0800

    update doc link

--- static/app-worker.js
+++ static/app-worker.js
@@ -1,8 +1,8 @@
-const cacheName = "app-" + "0.31";
+const cacheName = "app-" + "0.32";
 const resourcesToCache = ["/niudour","/niudour/app.css","/niudour/app.js","/niudour/manifest.webmanifest","/niudour/wasm_exec.js","/niudour/web/app.wasm","images/gopher.png","js/alert.js","js/editor.js","js/lib/monaco-editor/vs/loader.js","js/lib/sweetalert2.min.js","style.css"];
 
 self.addEventListener("install", (event) => {
-  console.log("installing app worker 0.31");
+  console.log("installing app worker 0.32");
 
   event.waitUntil(
     caches
@@ -28,7 +28,7 @@ self.addEventListener("activate", (event) => {
       );
     })
   );
-  console.log("app worker 0.31 is activated");
+  console.log("app worker 0.32 is activated");
 });
 
 self.addEventListener("fetch", (event) => {
